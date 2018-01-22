9dfc85deac8b2efcdedc5d2ecc4ed27d623eee76
keke <zhoukekestar@163.comm>
2018-01-22 21:22:12 +0800

    add davinci

--- src/replace.js
+++ src/replace.js
@@ -1,20 +1,22 @@
-var abcdef;
-var abcdef;
-var abcdef;
-
-const funs = [];
-
-funs.push(text => {
-  return text.replace(/abcdef/g, 'aaa');
-})
+const vscode = require('vscode');
+const replaceDavinci = require('./replace-davinci');
 
 exports.execute = function(args) {
-  const vscode = require('vscode');
-  let text = vscode.window.activeTextEditor.document.getText();
 
-  for (let i = 0; i < funs.length; i++) {
-    text = funs[i](text);
-  }
+
+
+  vscode.window.showInformationMessage('Davinci replace finished!');
+
+// console.log('global', Object.keys(global));
+// console.log('replace', Object.keys(replaceDavinci))
+// console.log(JSON.stringify(replaceDavinci))
+
+  let text = vscode.window.activeTextEditor.document.getText();
+  const fileName = vscode.window.activeTextEditor.document.fileName;
+
+  console.log('execute')
+  // replace editor text
+  // text = replaceDavinci.replace(text, fileName);
 
   vscode.window.activeTextEditor.edit(editBuilder => {
     editBuilder.replace(new vscode.Range(0,0,99999,99999), text);
