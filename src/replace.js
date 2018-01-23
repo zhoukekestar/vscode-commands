54967831bffbd7efb00dbac43ae4907dd215448b
keke <zhoukekestar@163.comm>
2018-01-23 09:39:26 +0800

    update davinci

--- src/replace.js
+++ src/replace.js
@@ -1,22 +1,21 @@
 const vscode = require('vscode');
-const replaceDavinci = require('./replace-davinci');
+
 
 exports.execute = function(args) {
-
-
-
-  vscode.window.showInformationMessage('Davinci replace finished!');
-
-// console.log('global', Object.keys(global));
-// console.log('replace', Object.keys(replaceDavinci))
-// console.log(JSON.stringify(replaceDavinci))
 
   let text = vscode.window.activeTextEditor.document.getText();
   const fileName = vscode.window.activeTextEditor.document.fileName;
 
-  console.log('execute')
-  // replace editor text
-  // text = replaceDavinci.replace(text, fileName);
+
+  // args.require('/Users/zhoukeke/workspace/GitHub/vscode-commands/src/replace-davinci.js')
+  // args.require('~/workspace/Github/vscode-commands/src/replace-davinci');
+  // vscode.window.showInformationMessage('Davinci replace finished!');
+  // console.log('global', Object.keys(global));
+  // console.log('replace', Object.keys(replaceDavinci))
+  // console.log(JSON.stringify(replaceDavinci))
+  // console.log('execute', text, fileName)
+  const replaceDavinci = require('./replace-davinci');
+  text = replaceDavinci(text);
 
   vscode.window.activeTextEditor.edit(editBuilder => {
     editBuilder.replace(new vscode.Range(0,0,99999,99999), text);
