dc8f9ecf3ea991ba236ede5c4f4c68f0822efad3
keke <zhoukekestar@163.comm>
2018-01-24 15:01:10 +0800

    add tools

---
+++ src/tools/index.js
@@ -0,0 +1,28 @@
+const vscode = require('vscode');
+const OPTIONS = {
+  EMPTY: ' --- 请选择 --- ',
+  TERMINAL: '终端执行',
+}
+
+exports.execute = function(args) {
+
+  // Get current text & fliename
+  let text = vscode.window.activeTextEditor.document.getText();
+  const fileName = vscode.window.activeTextEditor.document.fileName;
+
+  // Pick one action to process
+  vscode.window.showQuickPick(Object.keys(OPTIONS).map(key => OPTIONS[key])).then(val => {
+    switch(val) {
+      case OPTIONS.TERMINAL:
+        const terminal = vscode.window.createTerminal('pipeTest');
+        terminal.sendText('ls');
+        terminal.sendText('cd ..');
+        terminal.sendText('ls');
+        terminal.show();
+        args.log('terminal')
+        break;
+      default:
+        args.log('Nothing Selected.');
+    }
+  })
+}
